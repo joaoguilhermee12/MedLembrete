5089b9d38e0f3183e3dd05b498fc3b7a52c7828d
joaoguilhermee12 <joaoguifigueiredo07@gmail.com>
2026-04-06 18:34:47 -0300

    Correcting H2

--- src/test/java/com/joaoguilhermee/MedLembrete/MedLembreteApplicationTests.java
+++ src/test/java/com/joaoguilhermee/MedLembrete/MedLembreteApplicationTests.java
@@ -2,12 +2,13 @@ package com.joaoguilhermee.MedLembrete;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class MedLembreteApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
-
 }
