396b757d6115380a092d2d9e3737e89d0b2a0c9e
joaoguilhermee12 <joaoguifigueiredo07@gmail.com>
2026-05-16 15:23:36 -0300

    Correcting Checkstyle

--- src/main/java/com/joaoguilhermee/MedLembrete/Model/DTO/DisifyResponseDTO.java
+++ src/main/java/com/joaoguilhermee/MedLembrete/Model/DTO/DisifyResponseDTO.java
@@ -1,17 +1,15 @@
 package com.joaoguilhermee.MedLembrete.Model.DTO;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Getter;
+import lombok.Setter;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
+@Getter
+@Setter
 public class DisifyResponseDTO {
     private boolean format;
     private boolean disposable;
     private boolean dns;
 
-    public boolean isFormat() { return format; }
-    public void setFormat(boolean format) { this.format = format; }
-    public boolean isDisposable() { return disposable; }
-    public void setDisposable(boolean disposable) { this.disposable = disposable; }
-    public boolean isDns() { return dns; }
-    public void setDns(boolean dns) { this.dns = dns; }
 }
